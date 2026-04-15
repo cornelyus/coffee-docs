4af1ea9a30dc35fa6395cc2917a0698ace9891b7
Ricardo Silva <cornelyus7@gmail.com>
2026-04-15 15:37:16 +0100

    Exception on proxy for not adding language suffix on url

--- proxy.ts
+++ proxy.ts
@@ -31,5 +31,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next|favicon.ico|api/).*)'],
+  matcher: ['/((?!_next|favicon.ico|api/|sitemap.xml|robots.txt|opengraph-image).*)'],
 }
